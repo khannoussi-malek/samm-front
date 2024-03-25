a72a85dc959559377537029aac2ea94c2ff8c7e3
mihedH <hassine.mihed2016@gmail.com>
2024-03-25 15:30:04 +0100

    add fieldInput
    Co-authored-by: Malek <khannoussi-malek@users.noreply.github.com>

--- src/components/FieldInput/index.tsx
+++ src/components/FieldInput/index.tsx
@@ -1,7 +1,46 @@
-import { Input } from "@chakra-ui/react";
-type FieldInputProps 
-export const FieldInput = (props) => {
+import { Input, InputProps } from "@chakra-ui/react";
+import { FormGroup, FormGroupProps } from "../FormGroup";
+import { FieldProps, useField } from '@formiz/core';
+
+
+type Value = InputProps['value'];
+
+type UsualInputProps = 'placeholder' | 'autoFocus' | 'type';
+
+export type FieldInputProps<FormattedValue = Value> = FieldProps<
+  Value,
+  FormattedValue
+> &
+  FormGroupProps &
+  Pick<InputProps, UsualInputProps> & {
+    inputProps?: Omit<InputProps, UsualInputProps>;
+  };
+
+export const FieldInput = <FormattedValue = Value>(props: FieldInputProps<FormattedValue>) => {
+  const field = useField(props);
+  const { inputProps, children, placeholder, type, autoFocus, ...rest } = field.otherProps;
+
+  const formGroupProps = {
+    ...rest,
+    errorMessage: field.errorMessage,
+    id: field.id,
+    isRequired: field.isRequired,
+    showError: field.shouldDisplayError,
+  }
+
   return (
-    <Input/>
+    <FormGroup {...formGroupProps}>
+      <Input
+        {...inputProps}
+        id={field.id}
+        value={field.value ?? ""}
+        type={type}
+        onChange={(e) => field.setValue(e.target.value)}
+        placeholder={placeholder ? String(placeholder) : ""}
+        onFocus={() => field.setIsTouched(false)}
+        onBlur={() => field.setIsTouched(true)}
+        autoFocus={autoFocus}
+      />
+    </FormGroup>
   );
 }
