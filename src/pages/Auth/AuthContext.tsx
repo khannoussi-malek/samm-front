1c32eac8393aa9f9630025b80a8358f376d4585d
mihedH <hassine.mihed2016@gmail.com>
2024-03-31 00:58:44 +0100

    AuthContext

--- src/pages/Auth/AuthContext.tsx
+++ src/pages/Auth/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { FC, PropsWithChildren, createContext, useCallback, useContext, useEffect, useMemo, useState } from "react";
 
 export type AuthContextType = {
     isAuthenticated : boolean ;
@@ -10,11 +10,40 @@ const AUTH_TOKEN_KEY= "token";
 const AuthContext = createContext<AuthContextType|null>(null);
 
 
-const  updateToken = (newToke?:string | null )=>{
+const  updateToken = (newToken?:string | null )=>{
     if (!newToken) {
         localStorage.removeItem(AUTH_TOKEN_KEY);
       } else {
         localStorage.setItem(AUTH_TOKEN_KEY, newToken);
       }
+}
 
+export const AuthProvider : FC<PropsWithChildren<unknown>> = ({children})=>{
+  const [token,setToken] = useState(localStorage.getItem(AUTH_TOKEN_KEY) || null);
+
+  const hundleUpdateToken =useCallback( (newToken :string) => {
+    setToken(newToken);
+    updateToken(newToken);
+  },[setToken]);
+
+  const value = useMemo(() => ({
+    isAuthenticated: !!token,
+    updateToken: hundleUpdateToken,
+  }),[token,hundleUpdateToken]);
+
+return(
+    <AuthContext.Provider value={value}>
+        {children}
+    </AuthContext.Provider>
+);
+};
+export const useAuthContext = ()=>{
+const [isLoading,setIsLoading] = useState(true);
+useEffect(()=>{setIsLoading(false)},[]);
+const context = useContext(AuthContext);
+if(!context){
+    throw new Error("useAuthContext must be used within AuthProvider");
+};
+const {isAuthenticated,updateToken} = context;
+return {isAuthenticated,updateToken,isLoading};
 }
