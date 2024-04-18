660edd6cf1556360ac1a8f3681bfcc91a1b8d0f7
mihedH <hassine.mihed2016@gmail.com>
2024-04-18 23:32:51 +0100

    forum retouches

--- src/pages/StudentList/StudentList.tsx
+++ src/pages/StudentList/StudentList.tsx
@@ -1,4 +1,4 @@
-import { Button, Table, TableCaption, TableContainer, Tbody, Td, Tfoot, Th, Thead, Tr } from "@chakra-ui/react";
+import { Box, Button, Center, Flex, Spinner, Table, TableCaption, TableContainer, Tbody, Td, Tfoot, Th, Thead, Tr } from "@chakra-ui/react";
 import { useStudentList } from "./students.service";
 
 export const StudentList = () => {
@@ -7,35 +7,44 @@ export const StudentList = () => {
 
 
     return (
-        <TableContainer h="100vh" display="flex" justifyContent="center" alignItems="center" bg="#017da7" flexDirection="column" >
-            <Button>add student</Button>
-            <Table variant='simple' color="#04326b" width="80%" bg={"#fff"} borderRadius={8}>
-                <TableCaption>Students' List</TableCaption>
-                <Thead>
-                    <Tr>
-                        <Th>name </Th>
-                        <Th>surname</Th>
-                        <Th>email</Th>
-                        <Th>phone</Th>
-                        <Th>cin/passeport</Th>
-                    </Tr>
-                </Thead>
-                {!!isLoading && <p>Loading...</p>}
-                {isError && <p>there is a error <Button onClick={() => refetch()}>Refresh</Button></p>}
-                <Tbody>
-                    {students.map((student) => (
-                        <Tr>
-                            <Td>{student.nom}</Td>
-                            <Td>{student.prenom}</Td>
-                            <Td>{student.email}</Td>
-                            <Td>{student.CIN}</Td>
-                            <Td>{student.phone}</Td>
-                        </Tr>
-                    ))};
-                </Tbody>
+        <Box bg="#017da7" minH="100vh" p="4">
+            <Center flexDirection="column" >
+                <Flex justifyContent="flex-end" w="80vw" mb="2">
+                    <Button>add student</Button></Flex>
+                {!!isLoading &&
+                    <Center w="full">
+                        <Spinner />
+                    </Center>}
+                {!isLoading && <TableContainer   >
+                    <Table minW="80vw" variant='simple' color="#04326b" width="80%" bg={"#fff"} borderRadius={8}>
+                        <TableCaption>Students' List</TableCaption>
+                        <Thead>
+                            <Tr>
+                                <Th>name </Th>
+                                <Th>surname</Th>
+                                <Th>email</Th>
+                                <Th>phone</Th>
+                                <Th>cin/passeport</Th>
+                            </Tr>
+                        </Thead>
 
-            </Table>
-        </TableContainer>
+                        {isError && <p>there is a error <Button onClick={() => refetch()}>Refresh</Button></p>}
+                        <Tbody>
+                            {students.map((student) => (
+                                <Tr>
+                                    <Td>{student.nom}</Td>
+                                    <Td>{student.prenom}</Td>
+                                    <Td>{student.email}</Td>
+                                    <Td>{student.CIN}</Td>
+                                    <Td>{student.phone}</Td>
+                                </Tr>
+                            ))}
+                        </Tbody>
+
+                    </Table>
+                </TableContainer>}
+            </Center>
+        </Box>
     );
 };
 
