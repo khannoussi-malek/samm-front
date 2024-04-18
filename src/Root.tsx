c4d36c2031c867cce656251d0339c55724cbdc04
mihedH <hassine.mihed2016@gmail.com>
2024-04-18 19:10:17 +0100

    Add StudentList component and integrate it into the router

--- src/Root.tsx
+++ src/Root.tsx
@@ -3,9 +3,10 @@ import App from './App';
 import { Login } from './pages/Auth/Login';
 import { SignUpStudent } from './pages/Auth/SignUpStudent';
 import { SignUpTeacher } from './pages/Auth/SignUpTeacher';
+import { StudentList } from './pages/StudentList/StudentList';
 
 const Root = () => {
-  const router = createBrowserRouter([{ path: '/', Component: App }, { path: '/login', Component: Login }, { path: '/signupStudent', Component: SignUpStudent},{path:'/signupTeacher',Component:SignUpTeacher}]);
+  const router = createBrowserRouter([{ path: '/', Component: App }, { path: '/login', Component: Login }, { path: '/signupStudent', Component: SignUpStudent},{path:'/signupTeacher',Component:SignUpTeacher},{ path: '/List', Component: StudentList}]);
   return <RouterProvider router={router} />;
 };
 
