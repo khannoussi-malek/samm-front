b062d34174d26525ee7e0eb864937ea0a11ed32d
mihedH <hassine.mihed2016@gmail.com>
2024-04-23 01:44:14 +0100

    correcting props names

--- src/components/CourseCard/index.tsx
+++ src/components/CourseCard/index.tsx
@@ -2,16 +2,16 @@ import { Stack, StackProps, Text } from "@chakra-ui/react";
 import { FC } from "react";
 
 type CourseCardProps = StackProps & {
-    chapNum: string;
-    chapTitle: string;
-    pagesnbr: string;
+    chapterNumber: string;
+    chapterTitle: string;
+    pagesNumber: string;
 }
-export const CourseCard: FC<CourseCardProps> = ({ chapNum, chapTitle, pagesnbr, ...rest }) => {
+export const CourseCard: FC<CourseCardProps> = ({ chapterNumber, chapterTitle, pagesNumber, ...rest }) => {
     return (
         <Stack p="4" flexDirection="column" justifyContent="center" minH="10rem" minW="15rem" bgColor="#01427A" borderRadius="32px" {...rest} >
-            <Text fontSize="lg" fontWeight="300" color="#FAFAFA"> {chapNum} </Text>
-            <Text fontSize="3xl" fontWeight="bold" color="#FAFAFA"> {chapTitle} </Text>
-            <Text fontSize="lg" fontWeight="300" color="#FAFAFA"> {pagesnbr} </Text>
+            <Text fontSize="lg" fontWeight="300" color="#FAFAFA"> {chapterNumber} </Text>
+            <Text fontSize="3xl" fontWeight="bold" color="#FAFAFA"> {chapterTitle} </Text>
+            <Text fontSize="lg" fontWeight="300" color="#FAFAFA"> {pagesNumber} </Text>
         </Stack>
     )
 }
